5a93528d0fa66652f558d7749e8f42be528199da
Ujjawal Tiwari <ujjawal140208@gmail.com>
2026-08-13 13:31:29 +0530

    new updates to lecture 5

--- lecture-5/index.js
+++ lecture-5/index.js
@@ -1,69 +1,96 @@
-// const os = require("os")
+// // const os = require("os")
 
-// console.log(os.platform())
+// // console.log(os.platform())
 
-// console.log(os.arch())
+// // console.log(os.arch())
 
-// console.log(os.hostname())
+// // console.log(os.hostname())
 
-// console.log(os.version())
+// // console.log(os.version())
 
-// console.log(os.uptime())
+// // console.log(os.uptime())
 
-// console.log(os.totalmem()/1024/1024/1024)
+// // console.log(os.totalmem()/1024/1024/1024)
 
-// console.log(os.freemem()/1024/1024/1024)
+// // console.log(os.freemem()/1024/1024/1024)
 
-// console.log(os.cpus())
+// // console.log(os.cpus())
 
-// console.log(os.cpus().length)
+// // console.log(os.cpus().length)
 
 const fs = require("fs")
 
-// fs.writeFile("data.txt","Hello world",(err)=>{ //agr mai yaha pe already written file ka name same rakhk ke run karu to already existing data overwrite kr jayega
+// // fs.writeFile("data.txt","Hello world",(err)=>{ //agr mai yaha pe already written file ka name same rakhk ke run karu to already existing data overwrite kr jayega
+// //     if (err) console.log(err)
+// //         else console.log("File Written")
+// // })
+
+// // fs.readFile("data.txt","utf8",(err,res)=>{
+// //     if (err) console.log(err)
+// //         else console.log(res)
+// // })
+
+// // fs.appendFile("data.txt","\n Rakesh ke ldka hua hai",(err)=>{
+// //     if (err) console.log(err)
+// //         else console.log("updated")
+// // })
+
+// // fs.unlink("data.txt",(err)=>{
+// //     if (err) console.log(err)
+// //         else console.log("inlinked")
+// // })
+
+// const data = {
+//     name: "Ujjawal",
+//     age: 19,
+//     city: "Dwarka",
+//     Country: "India"
+// }
+// // fs.writeFile("data.json",JSON.stringify(data,null,2), (err)=>{
+// //     if (err) console.log(err)
+// //         else console.log("created")
+// // })
+
+// const new_data = {
+//     name: "rakesh"
+// }
+
+// fs.readFile("data.json", "utf8", (err, res) => {
 //     if (err) console.log(err)
-//         else console.log("File Written")
+//     else {
+//         let temp = JSON.parse(res)
+//         temp.push(new_data)
+
+//         fs.writeFile("data.json", JSON.stringify(temp, null, 2), (err) => {
+//             if (err) console.log(err)
+//             else console.log("file updated")
+//         })
+//     }
 // })
 
-// fs.readFile("data.txt","utf8",(err,res)=>{
-//     if (err) console.log(err)
-//         else console.log(res)
-// })
+const path = require("path")
+const { fileURLToPath } = require("url")
 
-// fs.appendFile("data.txt","\n Rakesh ke ldka hua hai",(err)=>{
-//     if (err) console.log(err)
-//         else console.log("updated")
-// })
+// const file = path.join("home","data","user.json")
 
-// fs.unlink("data.txt",(err)=>{
-//     if (err) console.log(err)
-//         else console.log("inlinked")
-// })
+// console.log(file)
 
-const data = {
-    name:"Ujjawal",
-    age:19,
-    city:"Dwarka",
-    Country:"India"
-}
-// fs.writeFile("data.json",JSON.stringify(data,null,2), (err)=>{
-//     if (err) console.log(err)
-//         else console.log("created")
-// })
+// console.log(path.dirname("home/user/data/file.txt")) // directory name dena hota hai na ki file name
 
-const new_data = {
-    name:"rakesh"
-}
+// console.log(path.basename("home/user/data/file.txt"))
 
-fs.readFile("data.json","utf8",(err,res)=>{
+// console.log(path.extname("home/user/data/file.txt"))
+
+const filepath = path.join("home", "data", "user", "file.txt")
+console.log(filepath)
+
+fs.mkdir(path.dirname(filepath), { recursive: true }, (err) => {
     if (err) console.log(err)
-        else{
-    let temp = JSON.parse(res)
-    temp.push(new_data)
-
-    fs.writeFile("data.json",JSON.stringify(temp,null,2),(err)=>{
-        if (err) console.log(err)
-            else console.log("file updated")
-    })
-        }
+    else {
+        fs.writeFile(filepath, "", (err) => {
+            if (err) console.log(err)
+            else console.log("done")
+        })
+    }
 })
+
