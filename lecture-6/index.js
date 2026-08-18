959139cb6e03b37dfe67ea5f6d99c63c938c226e
Ujjawal Tiwari <ujjawal140208@gmail.com>
2026-08-18 12:54:46 +0530

    lecture 6 updates for env

---
+++ lecture-6/index.js
@@ -0,0 +1,21 @@
+// // console.log(process.pid)
+
+// // console.log(process.ppid)
+
+// // console.log(process.uptime())
+
+// // let a = process.argv[0]
+// // console.log(a)
+
+
+// function addition(a,b){
+//     return Number(a)+Number(b);
+// }
+
+// console.log(addition(process.argv[2],process.argv[3]))
+
+
+// process.enviorement variable
+
+console.log(process.env.password)
+
